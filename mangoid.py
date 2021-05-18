1d87763ec0de8b0071950e7dafe4dc8e0019f2b2
mango <adamtamers@outlook.com>
2021-05-18 13:28:00 +0400

    Create mangoid.py

---
+++ mangoid.py
@@ -0,0 +1,16 @@
+# INSERT IMPORTING OF DATABASE MODULES
+
+def createnewid(id, points):
+  # INSERT DATABASE CODE HERE
+  
+def editID(id):
+  # INSERT EDIT ID CODE HERE
+  
+def subtractPoints(id, points):
+  # INSERT SUBTRACT POINTS HERE
+  
+def addPoints(id, points):
+  # INSERT ADD POINTS CODE HERE
+  
+def removeID(id):
+  # INSERT REMOVE ID CODE HERE
